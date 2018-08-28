a06f88835049213f29f9a66a4db51917dca60173
johndlopez <johnlopez@Johns-MacBook-Pro-2.local>
2018-08-27 19:26:51 -0700

    added db helper methods

--- db/mongo.js
+++ db/mongo.js
@@ -1,30 +1,86 @@
 var mongoose = require('mongoose');
 var ObjectId = mongoose.Schema.Types.ObjectId;
 
-mongoose.connect('http://localhost:2233/arena');
+mongoose.connect('mongodb://localhost:27017/arena');
 
 const db = mongoose.connection;
 db.on('error', () => console.log('Error connecting to mongo'));
 db.once('open', () => console.log('Connected to mongo'));
 
+
 const messageSchema = mongoose.Schema({
   messageId: ObjectId,
-  userName: { type: String, unique: True },
+  userName: String,
   message: String,
   time: Date,
   address: String,
-  xCoord: Number,
-  yCoord: Number
+  // xCoord: Number,
+  // yCoord: Number
   //maybe store users nearby that can receive message
 });
 
-const Message = mongoose.model('Message', messageSchema);
+const roomSchema = mongoose.Schema({
+  _id: Number,
+  messages: [messageSchema],
+});
+
+const Room = mongoose.model('Room', roomSchema);
 
 //Write out the message methods.
-const saveMessage;
-const getMessage;
+const saveMessage = (message, arenaId, callback) => {
+  // const arenaId = message._arenaId;
+  Room.updateOne(
+    { _id: arenaId },
+    { $push: { messages : message } },
+    callback
+  );
+};
+
+const getExistingMessages = (arenaId, callback) => {
+  Room.findOne({ _id: arenaId }, (error, room) => {
+    if (error) {
+      console.log(error);
+    } 
+    else {
+      let messages = room.messages;
+      let sorted = messages.sort({time: 1}).reverse();
+      callback(null, JSON.stringify(sorted, null, 2));
+    }
+  })
+};
+
+let testMessages = [{
+  messageId: mongoose.Types.ObjectId(),
+  userName: 'Res',
+  message: 'Oh hi again',
+  time: new Date(),
+  address: '222 Market St.',
+}]
+
+let testRoom = {
+  _id: 3,
+  messages: testMessages
+}
+
+//getExistingMessages test
+getExistingMessages(3, (err, messages) => {
+  if (err) {
+    console.log('E',err)
+  } else {
+    console.log('CB S', messages);
+  }
+})
+
+// Save message in server
+// saveMessage(testMessages, 3, (error, message) => {
+//   if (error) return error;
+//   console.log(message);
+// })
+
+// getExistingMessages(3, null)
+// console.log(db.collection('rooms'))
 
 module.exports = {
   saveMessage,
-  getMessage
+  getExistingMessages
 };
